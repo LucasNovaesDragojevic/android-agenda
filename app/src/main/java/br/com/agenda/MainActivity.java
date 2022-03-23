208baa2b5d4ee7b168b5a98380f685287b3a31d7
LucasNovaesDragojevic <lucas.dragojevic@gmail.com>
2022-03-23 18:22:27 -0300

    Added list of contacts

--- app/src/main/java/br/com/agenda/MainActivity.java
+++ app/src/main/java/br/com/agenda/MainActivity.java
@@ -2,15 +2,27 @@ package br.com.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Hellow World", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Tahiana"));
+        ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaAlunos.setAdapter(
+                new ArrayAdapter<>(
+                        this,
+                        android.R.layout.simple_list_item_1,
+                        alunos));
     }
 }
