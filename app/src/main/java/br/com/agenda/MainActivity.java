b68703162004cdb77acc501ec470a89ce30e555b
LucasNovaesDragojevic <lucas.dragojevic@gmail.com>
2022-03-24 12:01:02 -0300

    Adding app bar

--- app/src/main/java/br/com/agenda/MainActivity.java
+++ app/src/main/java/br/com/agenda/MainActivity.java
@@ -1,21 +1,22 @@
 package br.com.agenda;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Contacts");
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Tahiana"));
         ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
