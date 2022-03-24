223e6730c1a0710ab2d270a631dd1c01eaf4f31d
LucasNovaesDragojevic <lucas.dragojevic@gmail.com>
2022-03-24 14:33:08 -0300

    Refactored packages

--- app/src/main/java/br/com/agenda/ui/activity/ListContactsActivity.java
+++ app/src/main/java/br/com/agenda/ui/activity/ListContactsActivity.java
@@ -1,4 +1,4 @@
-package br.com.agenda;
+package br.com.agenda.ui.activity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
@@ -11,15 +11,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.agenda.R;
+
+public class ListContactsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Contacts");
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_contacts);
         List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "Jose", "Tahiana"));
-        ListView listaAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        ListView listaAlunos = findViewById(R.id.activity_list_contacts_listview);
         listaAlunos.setAdapter(
                 new ArrayAdapter<>(
                         this,
